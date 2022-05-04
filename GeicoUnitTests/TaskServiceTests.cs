af4807da5133ed378b8c722e31b5eee31351fbf1
bemahesh <bemahesh@users.noreply.github.com>
2022-05-04 00:39:00 -0400

    Unit tests fixes v1

--- GeicoUnitTests/TaskServiceTests.cs
+++ GeicoUnitTests/TaskServiceTests.cs
@@ -10,38 +10,92 @@ using T = Geico.Models;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using Geico.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GeicoUnitTests
 {
     public class TestControllerTestsNew
     {
+        Mock<ITasksService> taskServiceMock;
+        Mock<ILogger<TaskController>> iLoggerMock;
+        TaskController tasksController;
+        public TestControllerTestsNew()
+        {
+            taskServiceMock = new Mock<ITasksService>();
+            iLoggerMock = new Mock<ILogger<TaskController>>();
+            tasksController = new TaskController(iLoggerMock.Object, taskServiceMock.Object);
+        }
         [Test]
         public void ReturnsOk()
         {
-            var taskServiceMock = new Mock<ITasksService>();
-            var iLoggerMock = new Mock<ILogger<TaskController>>();
-            taskServiceMock.Setup(p => p.GetTasks()).Returns(
-                new List<T.Task>()
-                { 
-                    new T.Task(1, "test", "test", DateTime.Now, DateTime.Now, DateTime.Now, T.Priority.Medium, T.Status.New)
-                }
-                );
+            taskServiceMock.Setup(p => p.GetTasks())
+                .Returns(new List<T.Task>(){ new T.Task(1, "test", "test", DateTime.Now, DateTime.Now, DateTime.Now, T.Priority.Medium, T.Status.New)});
 
-            TaskController tasksController = new TaskController(iLoggerMock.Object, taskServiceMock.Object);
             var result = tasksController.Get();
+            OkObjectResult resultOk = (OkObjectResult)result;
 
-            Assert.True(true);
+            Assert.True(resultOk.StatusCode == 200);
         }
-        //public void ReturnsOk()
-        //{
 
-        //    // Arrange
-        //    var loggerMock = new Mock<ILogger<TasksService>>();
-        //    var taskService = new TasksService(loggerMock.Object);
+        [Test]
+        public void CreateNewTask()
+        {
+            var taskToCreate = new T.Task(0, "test", "test", DateTime.Now, DateTime.Now, DateTime.Now, T.Priority.Medium, T.Status.New);
+            var taskToReturn = new T.Task(11, "test", "test", DateTime.Now, DateTime.Now, DateTime.Now, T.Priority.Medium, T.Status.New);
 
-        //    var lists = taskService.GetTasks();
-        //    Assert.NotNull(lists);
-        //}
+            taskServiceMock.Setup(p => p.CreateTask(taskToCreate)).Returns(taskToReturn);
+
+            var result = tasksController.Create(taskToCreate);
+            OkObjectResult resultOk = (OkObjectResult)result;
+
+            Assert.True(resultOk.StatusCode == 200);
+
+            var returnedTask = (T.Task) resultOk.Value;
+
+            Assert.True(returnedTask.Id == taskToReturn.Id);
+        }
+
+        [Test]
+        public void CreateNewTask_PastDueDate()
+        {
+            DateTime dueDate = DateTime.Now.AddDays(-1);
+            var taskToCreate = new T.Task(0, "test", "test", dueDate, DateTime.Now, DateTime.Now, T.Priority.Medium, T.Status.New);
+            taskServiceMock.Setup(p => p.CreateTask(taskToCreate));
+
+            var result = tasksController.Create(taskToCreate);
+            StatusCodeResult resultOk = (StatusCodeResult)result;
+
+            Assert.True(resultOk.StatusCode == 400);
+        }
+
+        [Test]
+        public void UpdateTask()
+        {
+            DateTime dueDate = DateTime.Now.AddDays(1);
+            var taskToUpdate = new T.Task(11, "test", "test", dueDate, DateTime.Now, DateTime.Now, T.Priority.Medium, T.Status.New);
+            var taskToReturn = new T.Task(11, "test", "test", dueDate, DateTime.Now, DateTime.Now, T.Priority.Medium, T.Status.New);
+
+            taskServiceMock.Setup(p => p.UpdateTask(taskToUpdate)).Returns(taskToReturn);
+
+            var result = tasksController.Update(taskToUpdate);
+            OkObjectResult resultOk = (OkObjectResult)result;
+            Assert.True(resultOk.StatusCode == 200);
+
+            var returnedTask = (T.Task)resultOk.Value;
+            Assert.True(returnedTask.Id == taskToReturn.Id);
+        }
+
+        [Test]
+        public void Update_Non_Existing_Task()
+        {
+            var taskToUpdate = new T.Task(0, "test", "test", DateTime.Now, DateTime.Now, DateTime.Now, T.Priority.Medium, T.Status.New);
+            taskServiceMock.Setup(p => p.UpdateTask(taskToUpdate));
+
+            var result = tasksController.Update(taskToUpdate);
+            StatusCodeResult resultOk = (StatusCodeResult)result;
+
+            Assert.True(resultOk.StatusCode == 400);
+        }
 
     }
 }
